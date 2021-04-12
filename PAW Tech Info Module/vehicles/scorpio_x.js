be8a41d4a62e5d225aa4a15462cfeacf872fa544
Anden Wieseler <ttechno.alternate@hotmail.com>
2021-04-12 12:52:50 -0500

    fix typo

--- PAW Tech Info Module/vehicles/scorpio_x.js
+++ PAW Tech Info Module/vehicles/scorpio_x.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../config.json');
+const config = require('./config.json');
 module.exports = {
     name: 'scorpio_x',
 	execute(message, args) {
--- PAW Tech Info Module/vehicles/tx_max.js
+++ PAW Tech Info Module/vehicles/tx_max.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../config.json');
+const config = require('./config.json');
 module.exports = {
     name: 'tx_max',
 	execute(message, args) {
