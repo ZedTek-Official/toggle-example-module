131e9d1f963cb3dd22d9d838ab69b1ec6624911c
Anden Wieseler <ttechno@icloud.com>
2021-08-29 21:16:03 -0500

    Create unwarn.js

---
+++ Admin Module/commands/unwarn.js
@@ -0,0 +1,17 @@
+const config = require('./config.json')
+module.exports = {
+    name: 'unwarn',
+    execute(message, args) {
+        const warn1 = config.warnRoles.warning1;
+        const warn2 = config.warnRoles.warning2;
+        const warn3 = config.warnRoles.warning3;
+        const mute = config.muteRole;
+        const beenMuted = config.beenMutedRole;
+        if (!message.mentions.users.first()) {
+            message.channel.send('Whoops! You need to specify a person to warn!')
+        }
+        else {
+            message.mentions.users.first().roles.remove(warn1, warn2, warn3)
+        }
+    },
+};
