9d812f34ec3b8737821da18af9f8e9d6a8b2aaa2
Anden Wieseler <ttechno@icloud.com>
2022-07-11 14:08:08 -0500

    new ban cmd

--- Admin Module/commands/ban.js
+++ Admin Module/commands/ban.js
@@ -3,10 +3,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 module.exports = {
 	data: new SlashCommandBuilder()
         .setName('ban')
-        .setDescription('Bans the specified user from the server'),
+        .setDescription('Bans the specified user from the server')
+        .addUserOption(option => option.setName('userToBan').setDescription('Mention a user to ban')),
 	async execute(interaction) {
-        const mentionedUser = message.mentions.members.first()
-        await interaction.reply({ content: `${mentionedUser} was banned.`, ephemeral: true })
-        mentionedUser.ban()
+        await interaction.reply({ content: `${interaction.options.getMentionable('userToBan')} was banned.`, ephemeral: true })
+        //interaction.options.getMentionable('userToBan').ban()
 	},
 }
