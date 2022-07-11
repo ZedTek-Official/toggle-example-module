35235d060fbbaea93f64161003c757001c2bf132
Anden Wieseler <ttechno@icloud.com>
2022-07-11 14:26:16 -0500

    new ban cmd

--- Admin Module/commands/ban.js
+++ Admin Module/commands/ban.js
@@ -4,9 +4,9 @@ module.exports = {
 	data: new SlashCommandBuilder()
         .setName('ban')
         .setDescription('Bans the specified user from the server')
-        .addUserOption(option => option.setName('mentionable').setDescription('Mention a user to ban')),
+        .addUserOption(option => option.setName('user').setDescription('Mention a user to ban')),
 	async execute(interaction) {
-        await interaction.reply({ content: `${interaction.options.getMentionable('mentionable')} was banned.`, ephemeral: true })
+        await interaction.reply({ content: `${interaction.options.getUser('user')} was banned.`, ephemeral: true })
         //interaction.options.getMentionable('userToBan').ban()
 	}
 }
