68d89012b0b55c55d1873e4fd573a620564569c5
Anden Wieseler <ttechno@icloud.com>
2022-07-13 13:46:45 -0500

    new ban cmd

--- Admin Module/commands/ban-new4.js
+++ Admin Module/commands/ban-new4.js
@@ -7,7 +7,7 @@ module.exports = {
         .setDescription('Bans the specified user from the server')
         .addUserOption(option => option.setName('user').setDescription('Mention a user to ban')),
 	async execute(interaction) {
-        if (member.permissions.has([Permissions.FLAGS.KICK_MEMBERS, Permissions.FLAGS.BAN_MEMBERS])) {        
+        if (interaction.user.permissions.has([Permissions.FLAGS.KICK_MEMBERS, Permissions.FLAGS.BAN_MEMBERS])) {        
             await interaction.reply({ content: `${interaction.options.getUser('user')} was banned.`, ephemeral: true })
             interaction.options.getMentionable('userToBan').ban()
         } 
